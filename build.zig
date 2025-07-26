edbdb759f893fa6b2a7b66cc3f1e6409291b1913
Jesperoka <jesperoka@gmail.com>
2025-07-26 19:07:14 +0200

    Move from battlebuds.

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const NAME = "rgbapng";
+    const SOURCE = "rgbapng.zig";
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const module = b.addModule(.{
+        .name = NAME,
+        .root_source_file = b.path(SOURCE),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    _ = module;
+}
